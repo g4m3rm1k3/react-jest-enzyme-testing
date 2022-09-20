6b7e6716453e491e9e303d4e000d963a8d9b4850
Michael McLean <g4m3rm1k3@hotmail.com>
2022-09-20 05:07:23 -0400

    extended CommentBox test to include form submit

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -16,10 +16,20 @@ it("has a text area and a button", () => {
 	expect(wrapper.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-	wrapper
-		.find("textarea")
-		.simulate("change", { target: { value: "new comment" } });
-	wrapper.update();
-	expect(wrapper.find("textarea").prop("value")).toEqual("new comment");
+describe("the text area", () => {
+	beforeEach(() => {
+		wrapper
+			.find("textarea")
+			.simulate("change", { target: { value: "new comment" } });
+		wrapper.update();
+	});
+	it("has a text area that users can type in", () => {
+		expect(wrapper.find("textarea").prop("value")).toEqual("new comment");
+	});
+
+	it("clears the text area when form submitted", () => {
+		wrapper.find("form").simulate("submit");
+		wrapper.update();
+		expect(wrapper.find("textarea").prop("value")).toEqual("");
+	});
 });
